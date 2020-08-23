b08f23f8da0599dfcb5f52365d91d4386c490026
Amanda Yu <wq2yu@uwaterloo.ca>
2020-08-23 03:41:36 -0400

    fixed nav bar links and spacing of margins

--- docs/main.js
+++ docs/main.js
@@ -1,109 +1,36 @@
-const logo = document.querySelectorAll('#applying__svg path')
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 
-console.log(logo);
+const http = require('http');
+const fs = require('fs');
+const con = require("./DBConnection");
 
-for (let i = 0; i<logo.length; i++){
-    console.log(`letter ${i} is ${logo[i].getTotalLength()}`)
-}
+const hostname = '127.0.0.1';
+const port = '3000';
 
+//using a static folder
+app.use(express.static('docs'));
 
-/*Scroll Reveal*/
-
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-})
-
-sr.reveal('.aif', {})
-
-sr.reveal('#aif-sub-1', {delay:200})
-sr.reveal('#aif-para-1', {delay:400})
-
-sr.reveal('#aif-sub-2', {delay:200})
-sr.reveal('#aif-para-2', {delay:400})
-
-sr.reveal('#aif-sub-3', {delay:200})
-sr.reveal('#aif-para-3', {delay:400})
-
-sr.reveal('#aif-ol-1', {delay:500})
-
-sr.reveal('#aif-para-3-add', {delay:600})
-
-sr.reveal('#aif-sub-4', {delay:200})
-sr.reveal('#aif-para-4', {delay:400})
-sr.reveal('.aif__histo', {delay:500})
-
-sr.reveal('#aif-sub-5', {delay:200})
-sr.reveal('#aif-para-5', {delay:400})
-sr.reveal('#aif-ul-1', {delay:500})
-
-sr.reveal('#aif-sub-6', {delay:200})
-sr.reveal('#aif-para-6', {delay:400})
-sr.reveal('#aif-ol-2', {delay: 500})
-
-sr.reveal('#aif-sub-7', {delay:200})
-sr.reveal('#aif-para-7', {delay:400})
-
-sr.reveal('#aif-sub-8', {delay:200})
-sr.reveal('#aif-para-8', {delay:400})
-sr.reveal('#aif-ul-2', {delay:500})
-
-
-/* Mobile Nav */
-
-/* --Media Query-- */
-
-const mq = window.matchMedia(" (max-width: 820px) ")
-
-/* --Nav Toggling-- */
-const hamburger = document.getElementById('hamburger');
-const header = document.getElementById('header');
-const nav = document.getElementById('nav-icon');
-const x = document.getElementById('nav-icon-x')
-
-
-if (mq.matches) {
-    hamburger.addEventListener('click', () =>{
-        if (header.style.display === 'none'){
-            header.style.display ='block';
-            nav.style.display = 'none';
-            x.style.display = 'block';
-        }else{
-            header.style.display = 'none';
-            nav.style.display = 'block';
-            x.style.display = 'none';
-        }
+//server connection to the discussion board
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/docs/discussion-board.html'));
+  
+  //hopefully query the database?
+  
+  var conn = con.getConnection();
+  conn.query("SELECT * FROM discussion.discussion", function (error, results, fields) {
+    if (error) throw error;
+    results.forEach((discussion) => {
+      console.log(discussion);
     });
-}else{
-    header.style.display = 'block';
-    nav.style.display = 'none';
-    x.style.display = 'none';
-}
-
-/* Changing hamburger nav to x */
-
-
-/* About Us page carousel */
-const slider = document.querySelector('.slider');
-const leftArrow = document.querySelector('.left');
-const rightArrow = document.querySelector('.right');
-
-var sectionIndex = 0;
-
-function setIndex(){
-
-    slider.style.transform = 'translate(' + (sectionIndex) * -33.3 + '%)';
-}
-
-
-leftArrow.addEventListener('click', function() {
-  sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
-  setIndex(sectionIndex);
+  });
+  conn.end();
 });
 
-rightArrow.addEventListener('click', function() {
-  sectionIndex = (sectionIndex < 2) ? sectionIndex + 1 : 2;
-  setIndex(sectionIndex);
-});
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log(`Server running at http://${hostname}:${port}/`);
